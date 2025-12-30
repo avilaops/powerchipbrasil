7c0899088400b95d7af757fd87357cbc32f77bb3
Nícolas Ávila <dev@avilaops.com>
2025-12-30 03:37:26 +0000

    Update API URL to use Cloudflare subdomain

--- config.js
+++ config.js
@@ -1,8 +1,7 @@
 // Configuração da API
-// Em produção, altere para a URL do Railway
 const API_URL = window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1'
     ? 'http://localhost:3000'
-    : 'https://powerchipbrasil-production.up.railway.app';
+    : 'https://api.avila.inc';
 
 // Exportar configuração
 window.CONFIG = {
--- server.js
+++ server.js
@@ -30,7 +30,7 @@ app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
     res.setHeader('Access-Control-Allow-Credentials', 'true');
-    
+
     if (req.method === 'OPTIONS') {
         return res.sendStatus(200);
     }
